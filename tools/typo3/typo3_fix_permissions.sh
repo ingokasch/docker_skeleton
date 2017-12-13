696bc89c2f05424cff88db4dbd46f0047f22cc2f
Ingo Kasch <ingo.kasch@psbrands.de>
2017-12-13 21:46:54 +0100

    [TASK] Add TYPO3 fix permission script

--- tools/typo3/typo3_fix_permissions.sh
+++ tools/typo3/typo3_fix_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "### Fixing permissions - LocalConfiguration.php"
-sudo chmod 777 /htdocs/spiegelau/spiegelauwebsiterelaunch/typo3conf/LocalConfiguration.php
-echo "### Fixing permissions - PackageStages.php"
-sudo chmod 777 /htdocs/spiegelau/spiegelauwebsiterelaunch/typo3conf/PackageStates.php
+echo "--> Fixing permissions - LocalConfiguration.php"
+docker-compose exec web-{projectName} /bin/bash -c 'chmod 777 /var/www/html/typo3conf/LocalConfiguration.php'
+echo "--> Fixing permissions - PackageStages.php"
+docker-compose exec web-{projectName} /bin/bash -c 'chmod 777 /var/www/html/typo3conf/PackageStates.php'
